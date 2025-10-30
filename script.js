d0edf277171b97e30481299b9c6584e76c5e9d2a
ZAAANE <91702939+ZaneAP@users.noreply.github.com>
2025-10-30 18:50:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ function previewImage(event) {
   const file = event.target.files[0];
   if (!file) return;
 
+  // Hide upload container
+  const uploadContainer = document.getElementById('uploadContainer');
+  uploadContainer.style.display = 'none';
+
   const scrollContainer = document.getElementById('scrollContainer');
 
+  // Uploaded image
   const uploaded = new Image();
-  uploaded.src = URL.createObjectURL(file); // faster than FileReader
+  uploaded.src = URL.createObjectURL(file);
   uploaded.classList.add('uploaded');
 
   scrollContainer.appendChild(uploaded);
